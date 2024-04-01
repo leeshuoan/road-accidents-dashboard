3e8fce2d0bdac8a36251f1420864734bdd4a3d02
Lee Shuoan <leeshuoan38@gmail.com>
2024-04-01 14:38:23 +0800

    feat: mrt tableau dashboard

--- src/Insights/PublicTransportInsights.tsx
+++ src/Insights/PublicTransportInsights.tsx
@@ -5,7 +5,11 @@ const PublicTransportInsights = () => {
     <>
       <div className="pt-10 pb-16 bg-[#F2F2F2]">
         <div className="flex justify-between gap-5 w-11/12 mx-auto">
-          <TableauEmbed url="https://public.tableau.com/views/BusDashboard_17117888450920/TrafficAccidentsDashboard?:language=en-GB&:sid=&:display_count=n&:origin=viz_share_link" width="4000px" height="700px" />
+          <TableauEmbed
+            url="https://public.tableau.com/views/Visualisations_17110383019890/BusDashboard?:language=en-GB&:sid=&:display_count=n&:origin=viz_share_link"
+            width="4200px"
+            height="700px"
+          />
           <div>
             <p className="mb-2 text-2xl font-bold">Singapore's Bus Network</p>
             <p className="mb-14 text-lg text-gray-600">
@@ -17,9 +21,34 @@ const PublicTransportInsights = () => {
             </p>
 
             <p className="mb-2 text-lg font-semibold">
-              The most passenger tap-ins are concentrated in Woodlands, Jurong West, Tampines
+              The most passenger tap-ins are concentrated in Woodlands, Jurong
+              West, Tampines
             </p>
           </div>
+        </div>
+      </div>
+      <div className="pt-10 pb-16 bg-[#F2F2F2]">
+        <div className="flex justify-between gap-5 w-11/12 mx-auto">
+          <div>
+            <p className="mb-2 text-2xl font-bold">Singapore's Mrt Network</p>
+            <p className="mb-14 text-lg text-gray-600">
+              Singapore's Mrt Network forms a significant part of public
+              transport in Singapore. Analysing this network could inform
+              policies and initiatives geared towards optimising public
+              transportation systems and potentially mitigate the impact of
+              increased vehicular traffic on road safety
+            </p>
+
+            <p className="mb-2 text-lg font-semibold">
+              The most passenger tap-ins are concentrated in Woodlands, Jurong
+              West, Tampines
+            </p>
+          </div>
+          <TableauEmbed
+            url="https://public.tableau.com/views/Visualisations_17110383019890/MRTDashboard?:language=en-GB&:sid=&:display_count=n&:origin=viz_share_link"
+            width="4200px"
+            height="700px"
+          />
         </div>
       </div>
     </>
